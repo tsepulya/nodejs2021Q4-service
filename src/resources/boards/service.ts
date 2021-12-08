af4ff5ce2c74c27e4ad6afa34ffd8eff2730b93d
tsepulya <tsepeleva_anna@mail.ru>
2021-12-08 14:59:56 +0300

    feat: add tsdocs in services files

--- src/resources/boards/service.ts
+++ src/resources/boards/service.ts
@@ -1,14 +1,39 @@
 import { boards } from "./db";
 import { Board } from "./types";
 
+/**
+ * Returns the array of boards
+ * @returns the array of boards
+ */
+
 export const getAllBoards = () => boards;
 
+/**
+ * Add a new board in the array of boards
+ * @param board - new board with type <Board>
+ * @returns Add board in the array of boards
+ */
+
 export const addInBoards = (board: Board) => boards.push(board);
+
+/**
+ * Delete board from the array of boards
+ * @param id - board`s id
+ * @returns Delete board with such id from array of boards
+ */
 
 export const deleteInBoards = (id: string) => {
     const index = boards.findIndex(item => item.id === id);
     boards.splice(index, 1);
 }
+
+/**
+ * Change board`s properties in the array of boards
+ * @param id - board`s id
+ * @param newBoard - board (type <Board>) with changed properties
+ * @returns change board`s properties in the array of boards
+ */
+
 
 export const changeInBoards = (id: string, newBoard: Board) => {
     const boardNew = boards.find(elem => elem.id === id);

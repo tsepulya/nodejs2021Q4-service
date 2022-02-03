28b7610544b47cb3efb35611159f2ddd734a5ba6
tsepulya <tsepeleva_anna@mail.ru>
2022-02-03 18:53:31 +0300

    feat: add USE_FASTIFY

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,33 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import { HOST, PORT, POSTGRES_HOST } from './common/config';
+import { HOST, PORT, POSTGRES_HOST, USE_FASTIFY } from './common/config';
 import { ADDRESS, DOCKER } from './common/constants';
 import { HttpExceptionFilter } from './exceptionFilters/exception.filters';
 
 const HOST_APP = process.env.NODE_ENV === DOCKER ? POSTGRES_HOST : HOST;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  // app.useGlobalFilters(new HttpExceptionFilter());
-  if (HOST_APP === POSTGRES_HOST) {
+  if (USE_FASTIFY) {
+    const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter()
+    );
+    app.useGlobalPipes(new ValidationPipe());
     await app.listen(PORT, ADDRESS);
+
   } else {
-    await app.listen(PORT);
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
+    // app.useGlobalFilters(new HttpExceptionFilter());
+    if (HOST_APP === POSTGRES_HOST) {
+      await app.listen(PORT, ADDRESS);
+    } else {
+      await app.listen(PORT);
+    }
   }
+
 }
 
 bootstrap();

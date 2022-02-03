7a2d7257cb66a62d8def71d4766969ba845bee0f
tsepulya <tsepeleva_anna@mail.ru>
2022-02-03 16:54:00 +0300

    fix: ForbiddenException

--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { CreateLoginDto } from './dto/create-login.dto';
 import { LoginSignToken } from './login.signToken';
 
@@ -7,23 +7,16 @@ export class LoginService {
   constructor(private readonly loginSignToken: LoginSignToken) {}
 
   async create(createLoginDto: CreateLoginDto) {
-    // return 'This action adds a new login';
 
     const { login, password } = createLoginDto;
 
     if (!login || !password || Object.keys(createLoginDto).length !== 2) {
-      // res.status(403).send('Wrong login/password combination');
-      // log.error('Wrong login/password combination');
-      // throw new CustomError('Wrong login/password combination', 403);
-      return 'Change to error';
+      throw new ForbiddenException('Wrong login/password combination');
     }
     const token = await this.loginSignToken.signToken(login, password);
 
     if (!token) {
-      // res.status(403).send('Wrong login/password combination');
-      // log.error('Wrong login/password combination');
-      // throw new CustomError('Wrong login/password combination', 403);
-      return 'Change to error';
+      throw new ForbiddenException('Wrong login/password combination');
     }
     return { token };
   }
